453c42c7ba0cb2d00f041b6f62c2bafb8a773044
edmondmw <edmondmwang@gmail.com>
2017-09-17 02:05:08 -0700

    Implemented wall jumping

--- Assets/Scripts/SimplePlatformController.cs
+++ Assets/Scripts/SimplePlatformController.cs
@@ -6,52 +6,81 @@ public class SimplePlatformController : MonoBehaviour {
 
 	[HideInInspector] public bool facingRight = true;
 	[HideInInspector] public bool jump = false;
+    [HideInInspector] public bool onWall = false;
 
 	public float moveForce = 365f;
 	public float maxSpeed = 5f;
-	public float jumpForce = 1000f;
+	public float verticalJumpForce = 1000f;
+    public float horizontalJumpForce;
 	public Transform groundCheck;
+    public Transform wallCheck;
 
 	private bool grounded = false;
 	private Animator anim;
 	private Rigidbody2D rb2d;
 
 	// Use this for initialization
-	void Awake () {
+	void Awake ()
+    {
 		anim = GetComponent<Animator> ();
 		rb2d = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		//Linecast returns true if it hits something in between
 		grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));
+        onWall = Physics2D.Linecast(transform.position, wallCheck.position, 1 << LayerMask.NameToLayer("Wall"));
 
-		if (Input.GetButtonDown ("Jump") && grounded) {
+        if (Input.GetButtonDown ("Jump") && (grounded || (!grounded && onWall)))
+        {
 			jump = true;
 		}
 	}
 
-	void FixedUpdate() {
+	void FixedUpdate()
+    {
 		float h = Input.GetAxis ("Horizontal");
 		//sets animator speed
 		anim.SetFloat ("Speed", Mathf.Abs (h));
 
-		if (h * rb2d.velocity.x < maxSpeed) {
-			rb2d.AddForce (Vector2.right * h * moveForce);
-		}
+        //Ground movement for walking
+        if (grounded)
+        {
+            if (h * rb2d.velocity.x < maxSpeed)
+            {
+                rb2d.AddForce(Vector2.right * h * moveForce);
+            }
 
-		if (Mathf.Abs (rb2d.velocity.x) > maxSpeed) {
-			rb2d.velocity = new Vector2 (Mathf.Sign (rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
-		}
+            if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
+            {
+                rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+            }
+        }
 
-		if ((facingRight && h < 0) || (!facingRight && h > 0)) {
+		if ((facingRight && h < 0) || (!facingRight && h > 0))
+        {
 			Flip ();
 		}
 
 		if (jump) {
-			anim.SetTrigger ("Jump");
-			rb2d.AddForce (new Vector2 (0f, jumpForce));
+            anim.SetTrigger("Jump");
+            if (grounded)
+            {
+                rb2d.AddForce(new Vector2(0f, verticalJumpForce));
+            } else
+            {
+                if(facingRight)
+                {
+                    rb2d.AddForce(new Vector2(-horizontalJumpForce, verticalJumpForce));
+                } else
+                {
+                    rb2d.AddForce(new Vector2(horizontalJumpForce, verticalJumpForce));
+                }
+                
+            }
+			
 			jump = false;
 		}
 
