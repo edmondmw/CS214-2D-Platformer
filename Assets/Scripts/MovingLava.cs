82e8b9738f7f714eed1ca6dc376785cbaecfc2dd
edmondmw <edmondmwang@gmail.com>
2017-09-17 22:29:49 -0700

    Destroy meteors on lava collision

--- Assets/Scripts/MovingLava.cs
+++ Assets/Scripts/MovingLava.cs
@@ -21,6 +21,9 @@ public class MovingLava : MonoBehaviour {
         if (collision.CompareTag("Player"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        } else if (collision.CompareTag("Meteor"))
+        {
+            Destroy(collision.gameObject);
         }
     }
 }
